822557038b425d34e823c906380a135d2618cdae
Pierre221352 <221352@virtualwindow.co.za>
2023-10-22 21:59:00 +0200

    Update main.js

--- script/main.js
+++ script/main.js
@@ -17,6 +17,10 @@ window.addEventListener('scroll', function() {
 
 // IMPLEMENTING tmdB movie api 
 
+const API_KEY = '?api_key=' + '94b6a2d34c3b54e7f76a308cedd0b6b3';
+const BASE_URL = 'https://api.themoviedb.org/3/movie/popular/';
+const IMAGE_URL = 'https://image.tmdb.org/t/p/w500';
+
 $(document).ready(function(){
     getMoviesByFirstLetter('a');
 });
@@ -43,9 +47,7 @@ function getMoviesByFirstLetter(letter) {
 
             displayMovies(movies);
         },
-        error: function(error) {
-            console.error('Error:', error);
-        }
+        
     });
 }
 
@@ -55,15 +57,18 @@ function displayMovies(movies) {
 
     movies.forEach(movie => {
         const card = $(`
-            <div class="col-lg-4 col-md-6 mb-4">
-                <div class="card">
-                    <img src="${movie.image}" class="card-img-top" alt="${movie.name}">
-                    <div class="card-body">
-                        <h5 class="card-title">${movie.name}</h5>
-                        <a href="#" class="btn btn-primary">View Movie</a>
-                    </div>
+        <div class="row watchlist-mov py-5">
+        <div class="col-3 d-flex align-items-stretch">
+            <div class="card rounded-4" style="width: 18rem;">
+                <img src="assets/images/movie card 9.png" class="card-img-top" alt="The Expendables 4">
+                <div class="card-body d-flex flex-column">
+                    <h4>The Expendables 4</h4>
+                    <h6>2023 - 1h 43min -Action/Adventure</h6>
+                    <p class="card-text">Director: Scott Waugh</p>
+                    <p class="text-body-tertiary rating">Rating - 4.9/10</p>
                 </div>
-            </div>`);
+            </div>
+        </div>`);
 
         card.click(function() {
             window.location.href = `movie.html?id=${movie.id}`;
