fe60be8976783addbf34e99900268eb94784fe35
Pierre221352 <221352@virtualwindow.co.za>
2023-10-22 17:16:12 +0200

    changed js

--- script/main.js
+++ script/main.js
@@ -5,12 +5,15 @@ window.addEventListener('scroll', function() {
     if (scrollPosition > 50) {
         body.style.backgroundColor = '#1A1A1A';
         document.querySelectorAll('h1').forEach(function(h1) {
-            h1.style.color = '#E21221'; 
+            h1.style.color = 'white'; 
         });
     } else {
         body.style.backgroundColor = 'white'; 
         document.querySelectorAll('h1').forEach(function(h1) {
-            h1.style.color = '#29485D'; 
+            h1.style.color = '#2C475A'; 
         });
     }
 });
+
+// IMPLEMENTING tmdB movie api 
+
