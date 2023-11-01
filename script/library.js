d3a8e7d9b5a393a3cc6aa986f7667473b8f06c3a
mwape-k <125279827+Mwape-Kurete@users.noreply.github.com>
2023-11-01 23:46:44 +0200

    fixing responsiveness

--- script/library.js
+++ script/library.js
@@ -221,9 +221,9 @@ function showMovieData(data) {
                             
                                 <!-- icons -->
                                 <div class="d-grid gap-2 d-md-flex justify-content-md-end icons-cont">
-                                    <button class="btn btn-warning me-md-2 btnPlay" type="button" onclick="getSingleMoviesId(this)"  id="${id}"><img
+                                    <button class="btn btn-warning me-md-2 btnPlay icon-btn" type="button" onclick="getSingleMoviesId(this)"  id="${id}"><img
                                             src="../assets/icons/play-circle-fill.svg" width="25px" height="25px" id="play"></button>
-                                    <button class="btn btn-warning me-md-2 btnBookmark" type="button" onclick="getMovieId(this)" id="${id}"><img
+                                    <button class="btn btn-warning me-md-2 btnBookmark icon-btn" type="button" onclick="getMovieId(this)" id="${id}"><img
                                             src="../assets/icons/bookmark-check.svg" width="25px" height="25px" ></button>
                                 </div>
                             </div>
