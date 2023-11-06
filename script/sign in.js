10156b68ffdf30ade5eff65fcc5fc1066b09726d
brilu-22 <231214@virtualwindow.co.za>
2023-11-06 11:38:27 +0200

    update

--- script/sign in.js
+++ script/sign in.js
@@ -1,100 +1,116 @@
 // Validation function
 function validateInputs() {
     let valid = true;
-
-    const usernameValue = $('#username').val().trim();
+  
     const emailValue = $('#email').val().trim();
     const passwordValue = $('#password').val().trim();
-    const password2Value = $('#password2').val().trim();
-
-    if (usernameValue === '') {
-        setError($('#username'), 'Username is required');
-        valid = false;
-    } else {
-        setSuccess($('#username'));
-    }
-
+  
     if (emailValue === '') {
-        setError($('#email'), 'Email is required');
-        valid = false;
+      setError($('#email'), 'Email is required');
+      valid = false;
     } else if (!isValidEmail(emailValue)) {
-        setError($('#email'), 'Provide a valid email address');
-        valid = false;
+      setError($('#email'), 'Please enter a valid email address');
+      valid = false;
     } else {
-        setSuccess($('#email'));
+      setSuccess($('#email'));
     }
-
+  
     if (passwordValue === '') {
-        setError($('#password'), 'Password is required');
-        valid = false;
+      setError($('#password'), 'Password is required');
+      valid = false;
     } else if (passwordValue.length < 6) {
-        setError($('#password'), 'Password must be at least 6 characters.');
-        valid = false;
+      setError($('#password'), 'Password must be at least 6 characters.');
+      valid = false;
     } else {
-        setSuccess($('#password'));
+      setSuccess($('#password'));
     }
-
-    if (password2Value === '') {
-        setError($('#password2'), 'Please confirm your password');
-        valid = false;
-    } else if (password2Value !== passwordValue) {
-        setError($('#password2'), "Passwords don't match");
-        valid = false;
-    } else {
-        setSuccess($('#password2'));
-    }
-
+  
     return valid;
-}
-
-// Email validation function
-function isValidEmail(email) {
+  }
+  
+  // Email validation function
+  function isValidEmail(email) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
-}
-
-// Error and success functions
-function setError(element, message) {
-    const inputControl = element.closest('.input-control');
-    const errorDisplay = inputControl.find('.error');
-
+  }
+  
+  // Error and success functions
+  function setError(element, message) {
+    const inputControl = element.closest('.form-group');
+    const errorDisplay = inputControl.find('.invalid-feedback');
+  
     errorDisplay.text(message);
-    inputControl.addClass('error');
-    inputControl.removeClass('success');
-}
-
-function setSuccess(element) {
-    const inputControl = element.closest('.input-control');
-    const errorDisplay = inputControl.find('.error');
-
+    inputControl.addClass('was-validated');
+  }
+  
+  function setSuccess(element) {
+    const inputControl = element.closest('.form-group');
+    const errorDisplay = inputControl.find('.invalid-feedback');
+  
     errorDisplay.text('');
-    inputControl.addClass('success');
-    inputControl.removeClass('error');
-}
+    inputControl.removeClass('was-validated');
+  }
+  
+  // Run code when the document loads
+  $(document).ready(function() {
+    // Sign-in form
+    $('#signInForm').submit(function(event) {
+      event.preventDefault();
+  
+      if (this.checkValidity() === false) {
+        event.stopPropagation();
+      } else {
+        // Add input field validation code
+        if (validateInputs()) {
+          // Add code to save the sign-in data to local storage or perform other actions
+          // For example, you can check credentials and then redirect to index.html
+          const email = $('#email').val().trim();
+          const password = $('#password').val().trim();
+  
+         // If authentication is successful, redirect to index.html
+    console.log('Authentication successful. Redirecting to index.html');
+    window.location.href = 'index.html';
+
+        }
+      }
+  
+      $(this).addClass('was-validated');
+    });
+  });
+  
+  
 
 // Run code when the document loads
-$(document).ready(function() {
-    // Sign-up form
-    $('#signupForm').submit(function(event) {
-        event.preventDefault();
-
-        if (this.checkValidity() === false) {
-            event.stopPropagation();
-        } else {
-            // Add input field validation code
-            if (validateInputs()) {
-                // Add code to save the sign-up data to local storage
-                const formData = {
-                    username: $('#username').val().trim(),
-                    email: $('#email').val().trim(),
-                    password: $('#password').val().trim(),
-                };
-                localStorage.setItem('signupData', JSON.stringify(formData));
-
-                // Redirect to the sign-in page
-                window.location.href = 'sign-in.html';
-            }
+$(document).ready(function () {
+    // Sign-in form
+    $('#signInForm').submit(function (event) {
+      event.preventDefault();
+  
+      if (this.checkValidity() === false) {
+        event.stopPropagation();
+      } else {
+        // Add input field validation code
+        if (validateInputs()) {
+          // Retrieve email and password
+          const email = $('#email').val().trim();
+          const password = $('#password').val().trim();
+  
+          // Add code to save the sign-in data to local storage
+          const signInData = {
+            email: email,
+            password: password,
+          };
+  
+          // Convert to JSON and store in local storage
+          localStorage.setItem('signInData', JSON.stringify(signInData));
+  
+          // If authentication is successful, redirect to index.html
+          console.log('Authentication successful. Redirecting to index.html');
+          window.location.href = 'index.html';
         }
-        $(this).addClass('was-validated');
+      }
+  
+      $(this).addClass('was-validated');
     });
-});
+  });
+  
